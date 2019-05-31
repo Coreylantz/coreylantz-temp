f484fa9153c3da00cc5d3b33c993d0d29d0411b2
Corey Lantz <clantz@ecentricarts.com>
2019-05-31 16:12:56 -0400

    moving files adding style.css

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,19 @@
 // service-worker.js
-self.addEventListener('install', function() {
-  console.log('Install!');
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          'assets/css/style.css',
+        ]
+      );
+    })
+  );
 });
 self.addEventListener("activate", event => {
   console.log('Activate!');
 });
 self.addEventListener('fetch', function(event) {
-  console.log('Fetch!', event.request);
+  // console.log('Fetch!');
 });
 
