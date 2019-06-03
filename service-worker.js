ae364037023806a8d918c2141a7023153e66c4a8
Corey Lantz <clantz@ecentricarts.com>
2019-06-03 09:25:51 -0400

    cache styles

--- service-worker.js
+++ service-worker.js
@@ -22,5 +22,19 @@ self.addEventListener("activate", event => {
 });
 self.addEventListener('fetch', function(event) {
   // console.log('Fetch!');
+  // Hard coded
+  caches.match('/assets/css/style.css').then(function(response) {
+    if (!response) throw Error("No data");
+    return response.json();
+  }).then(function(data) {
+    // don't overwrite newer network data
+    if (!networkDataReceived) {
+      updatePage(data);
+    }
+  }).catch(function() {
+    // we didn't get cached data, the network is our last hope:
+    return networkUpdate;
+  }).catch(showErrorMessage).then(stopSpinner());
+
 });
 
