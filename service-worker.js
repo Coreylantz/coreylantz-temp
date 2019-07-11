93f48129fb0ddd1092c63d34a3744e7be6cd2827
Corey Lantz <corey.lantz12@gmail.com>
2019-07-10 20:53:00 -0400

    updating service worker

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,19 @@
 // service-worker.js
+const cacheName = 'coreyCache';
+const staticAssets = [
+  '/assets/css/style.css',
+  '/assets/fonts/Rockwell-Regular-Font.ttf',
+  '/assets/fonts/Rockwell-Regular-Font.woff',
+  '/assets/fonts/Rockwell-Regular-Font.woff2',
+  '/assets/fonts/unicodefuturab.ttf',
+  '/assets/fonts/unicodefuturab.woff',
+  '/assets/fonts/unicodefuturab.woff2',
+];
+
 self.addEventListener('install', function(event) {
-  const cacheName = 'coreyCache';
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        [
-          '/assets/css/style.css',
-          '/assets/fonts/Rockwell-Regular-Font.ttf',
-          '/assets/fonts/Rockwell-Regular-Font.woff',
-          '/assets/fonts/Rockwell-Regular-Font.woff2',
-          '/assets/fonts/unicodefuturab.ttf',
-          '/assets/fonts/unicodefuturab.woff',
-          '/assets/fonts/unicodefuturab.woff2',
-        ]
-      );
+      return cache.addAll(staticAssets);
     })
   );
 });
@@ -21,21 +21,11 @@ self.addEventListener("activate", event => {
   console.log('Activate!');
 });
 self.addEventListener('fetch', function(event) {
-  // console.log('Fetch!');
-  // Hard coded
-  caches.match('/assets/css/style.css').then(function(response) {
-    console.log(response);
-    if (!response) throw Error("No data");
-    return response.json();
-  }).then(function(data) {
-    // don't overwrite newer network data
-    if (!networkDataReceived) {
-      updatePage(data);
-    }
-  }).catch(function() {
-    // we didn't get cached data, the network is our last hope:
-    return networkUpdate;
-  }).catch(showErrorMessage).then(stopSpinner());
-
+  const url = new URL(event.request.url);
+  if (staticAssets.indexOf(url.pathname) !== -1) {
+    // url.pathname is a string relative to the site's root, 
+    console.log(event.request.url);
+    event.respondWith(caches.match(event.request));
+  }
 });
 
